0dc25b96acc01a352f2969e6d1a5f5ab8410c44f
Neo-Ayush-jha <ayush91176@gmail.com>
2025-11-14 23:35:06 +0530

    update

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://university-backend-pbmo.onrender.com/";
 
 export async function fetchUniversities() {
   const res = await fetch(`${API_BASE}/universities/`);
